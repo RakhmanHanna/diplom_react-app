3b679e480cd0e27c4fa90acff1a6586a4b0d7b79
rakhmanhanna <rakhmanhanna@gmail.com>
2021-10-12 14:26:27 +0300

    start

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+//import React from 'react';
+import Header from '../src/components/blocks/Navbar/navbar';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/blocks/Navbar/navbar.tsx
@@ -0,0 +1,14 @@
+import './navbar.css';
+
+const Header = () => {
+  return (
+    <div className="header-navbar">
+      <a href=" ">Posts</a>
+      <a href=" ">Albums</a>
+      <a href=" ">Todos</a>
+      <a href=" ">Users</a>
+    </div>
+  );
+};
+
+export default Header;
